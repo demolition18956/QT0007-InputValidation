dc9e2ff431adb544c3fc3df4a7080a32b82aa803
Demola Ayokanmbi <aoa0006@uah.edu>
2020-09-08 18:25:46 -0500

    Using Input Validators and RegEx patterns to exercise input limitations

---
+++ dialog.cpp
@@ -0,0 +1,41 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+#include <QIntValidator>
+#include <QRegularExpressionValidator>
+#include <QDoubleValidator>
+
+Dialog::Dialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+
+    QIntValidator* intValidator = new QIntValidator(ui->intLineEdit);
+    ui->intLineEdit->setValidator(intValidator);
+
+    QIntValidator* byteIntValidator = new QIntValidator(0,255,ui->intSmallLineEdit);
+    ui->intSmallLineEdit->setValidator(byteIntValidator);
+
+    QRegularExpressionValidator* binaryValidator = new QRegularExpressionValidator(QRegularExpression("[01]{1,8}"),ui->binLineEdit);
+    ui->binLineEdit->setValidator(binaryValidator);
+
+    QRegularExpressionValidator* hexValidator = new QRegularExpressionValidator(QRegularExpression("[0-9A-Fa-f]{1,2}"),ui->hexLineEdit);
+    ui->hexLineEdit->setValidator(hexValidator);
+
+    QDoubleValidator* floatValidator = new QDoubleValidator(-1000.0,1000.0,1,ui->floatLineEdit);
+    ui->floatLineEdit->setValidator(floatValidator);
+
+    QRegularExpressionValidator* lowerValidator = new QRegularExpressionValidator(QRegularExpression("[a-z]{1,15}"),ui->lowerLineEdit);
+    ui->lowerLineEdit->setValidator(lowerValidator);
+
+    QRegularExpressionValidator* upOrLowValidator = new QRegularExpressionValidator(QRegularExpression("[A-Za-z]{1,15}"),ui->upOrLowLineEdit);
+    ui->upOrLowLineEdit->setValidator(upOrLowValidator);
+
+    QRegularExpressionValidator* phoneValidator = new QRegularExpressionValidator(QRegularExpression("[0-9]{3}-[0-9]{3}-[0-9]{4}"),ui->phoneLineEdit);
+    ui->phoneLineEdit->setValidator(phoneValidator);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
